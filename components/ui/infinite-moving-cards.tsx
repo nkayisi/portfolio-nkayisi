a8507d9ba939ac1c03486a1e0765f39819b14165
Nelson Kayisirirya <nelson.kayisirirya@gmail.com>
2025-07-08 01:28:46 +0100

    Comment [Recommendations section]

--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -82,7 +82,7 @@ export const InfiniteMovingCards = ({
       <ul
         ref={scrollerRef}
         className={cn(
-          "flex w-max min-w-full shrink-0 flex-nowrap gap-4 py-4",
+          "flex sm:overflow-visible overflow-auto w-max min-w-full shrink-0 flex-nowrap gap-4 py-4",
           start && "animate-scroll",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
